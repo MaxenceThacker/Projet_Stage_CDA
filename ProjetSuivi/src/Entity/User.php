d7997fe09a3a86aebc6046119600351f0fba7245
MaxenceThacker <maxence.thacker@hotmail.fr>
2022-04-13 11:20:39 +0200

    update

--- ProjetSuivi/src/Entity/User.php
+++ ProjetSuivi/src/Entity/User.php
@@ -10,7 +10,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\Table(name: '`user`')]
-#[UniqueEntity(fields: ['emailUser'], message: 'There is already an account with this emailUser')]
+#[UniqueEntity(fields: ['emailUser'], message: 'Un compte existe déja avec cette adresse email')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
--- ProjetSuivi/src/Form/RegistrationFormType.php
+++ ProjetSuivi/src/Form/RegistrationFormType.php
@@ -5,6 +5,8 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
 use Symfony\Component\Form\Extension\Core\Type\TelType;
@@ -21,38 +23,39 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('emailUser')
-            ->add('agreeTerms', CheckboxType::class, [
-                'mapped' => false,
-                'constraints' => [
-                    new IsTrue([
-                        'message' => 'You should agree to our terms.',
-                    ]),
+            ->add('emailUser', EmailType::class, ['label' => 'Adresse email : '])
+            ->add('plainPassword', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'first_options' => [
+                    'attr' => ['autocomplete' => 'new-password'],
+                    'constraints' => [
+                        new NotBlank([
+                            'message' => 'Veuillez rentrez un mot de passe',
+                        ]),
+                        new Length([
+                            'min' => 6,
+                            'minMessage' => 'Votre mot de passe doit fait au moins {{ limit }} caractères',
+                            // max length allowed by Symfony for security reasons
+                            'max' => 4096,
+                        ]),
+                    ],
+                    'label' => 'Nouveau mot de passe : ',
                 ],
-            ])
-            ->add('plainPassword', PasswordType::class, [
-                // instead of being set onto the object directly,
+                'second_options' => [
+                    'attr' => ['autocomplete' => 'new-password'],
+                    'label' => 'Confirmation mot de passe : ',
+                ],
+                'invalid_message' => 'Le mot de passe doit correspondre au premier.',
+                // Instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
-                'constraints' => [
-                    new NotBlank([
-                        'message' => 'Please enter a password',
-                    ]),
-                    new Length([
-                        'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
-                        // max length allowed by Symfony for security reasons
-                        'max' => 4096,
-                    ]),
-                ],
             ])
-            ->add('nomUser', TextType::class, ['label' => 'Nom'])
-            ->add('prenomUser', TextType::class, ['label' => 'Prénom'])
-            ->add('ddnUser', BirthdayType::class, ['label' => 'Date de naissance'])
+            ->add('nomUser', TextType::class, ['label' => 'Nom : '])
+            ->add('prenomUser', TextType::class, ['label' => 'Prénom : '])
+            ->add('ddnUser', BirthdayType::class, ['label' => 'Date de naissance : '])
             ->add('adresseUser', TextareaType::class, ['label' => 'Adresse'])
-            ->add('compltAdresseUser', TextareaType::class, ['label' => 'Complément d\'adresse'])
-            ->add('numTelUser', TelType::class, ['label' => 'Numéro de téléphone'])
+            ->add('compltAdresseUser', TextareaType::class, ['label' => 'Complément d\'adresse : '])
+            ->add('numTelUser', TelType::class, ['label' => 'Numéro de téléphone : '])
         ;
     }
 
